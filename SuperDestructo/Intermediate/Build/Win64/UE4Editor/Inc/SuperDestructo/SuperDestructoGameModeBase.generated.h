1c3ba43feacc5a187d765571c75f8463e6126b1d
khadeidredean <khadeidredean@gmail.com>
2017-11-21 12:44:18 -0700

    hi

---
+++ SuperDestructo/Intermediate/Build/Win64/UE4Editor/Inc/SuperDestructo/SuperDestructoGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SUPERDESTRUCTO_SuperDestructoGameModeBase_generated_h
+#error "SuperDestructoGameModeBase.generated.h already included, missing '#pragma once' in SuperDestructoGameModeBase.h"
+#endif
+#define SUPERDESTRUCTO_SuperDestructoGameModeBase_generated_h
+
+#define SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_RPC_WRAPPERS
+#define SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASuperDestructoGameModeBase(); \
+	friend SUPERDESTRUCTO_API class UClass* Z_Construct_UClass_ASuperDestructoGameModeBase(); \
+public: \
+	DECLARE_CLASS(ASuperDestructoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/SuperDestructo"), NO_API) \
+	DECLARE_SERIALIZER(ASuperDestructoGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASuperDestructoGameModeBase(); \
+	friend SUPERDESTRUCTO_API class UClass* Z_Construct_UClass_ASuperDestructoGameModeBase(); \
+public: \
+	DECLARE_CLASS(ASuperDestructoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/SuperDestructo"), NO_API) \
+	DECLARE_SERIALIZER(ASuperDestructoGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASuperDestructoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASuperDestructoGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASuperDestructoGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASuperDestructoGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASuperDestructoGameModeBase(ASuperDestructoGameModeBase&&); \
+	NO_API ASuperDestructoGameModeBase(const ASuperDestructoGameModeBase&); \
+public:
+
+
+#define SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASuperDestructoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASuperDestructoGameModeBase(ASuperDestructoGameModeBase&&); \
+	NO_API ASuperDestructoGameModeBase(const ASuperDestructoGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASuperDestructoGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASuperDestructoGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASuperDestructoGameModeBase)
+
+
+#define SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_12_PROLOG
+#define SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_RPC_WRAPPERS \
+	SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_INCLASS \
+	SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SuperDestructo_Source_SuperDestructo_SuperDestructoGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ SuperDestructo/Source/SuperDestructo/SuperDestructoGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SuperDestructoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERDESTRUCTO_API ASuperDestructoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
